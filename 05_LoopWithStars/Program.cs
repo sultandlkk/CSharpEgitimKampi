0ee525490df0b963ea1e2419bfb9c11bdb117ab8
sultandlkk <168722025+sultandlkk@users.noreply.github.com>
2024-11-27 20:59:21 +0300

    Add files via upload

---
+++ 05_LoopWithStars/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_LoopWithStars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region ALT ALTA YILDIZ OLUŞTURMA
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine("*");
+            //}
+            //#endregion
+            //#region ALT ALTA YILDIZ OLUŞTURMA
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine("*");
+            //}
+            #endregion
+            #region YAN YANA YILDIZ OLUŞTURMA
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.Write("*");
+            //}
+            #endregion
+            #region ALT ALTA VE YAN YANA YILDIZ OLUŞTURMA
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine("**********");
+            //}
+            #endregion
+            #region DİK ÜÇGEN
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 1; j < i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+            #region TERS ÜÇGEN
+            //for (int i = 5; i >= 1; i--)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+            #region     DİK VE TERS ÜÇGEN BİR ARADA
+            //for (int i = 1; i <=5; i++)
+            //{
+            //    for (int j = 1; j <=i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //for (int l = 4; l >= 1; l--)
+            //{
+            //    for (int m = 1; m <= l; m++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+            #region PİRAMİT
+            //int n = 5;
+            //for (int i = 1; i<=n; i++)
+            //{
+            //    //boşluklar için
+            //    for (int j = n-i; j >0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    //yıldız için
+            //    for (int k = 1; k <= 2*i-1; k++)
+            //    {
+            //        Console.Write("*");
+
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+            #region TERS PİRAMİT
+            //int n = 5;
+            //for (int i = n; i>=1; i--)
+            //{
+            //    //boşluklar için
+            //    for (int j = n - i; j > 0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    //yıldız için
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
